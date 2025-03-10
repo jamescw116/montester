42ca7d419ce96573eef80bf829b87a1d6a87f16b
unknown <greatelleryjames@yahoo.com.hk>
2025-03-09 23:59:46 -0700

    Allow keyboard, change mouse move to show/hide control, add instructions

--- src/app/Bar.tsx
+++ src/app/Bar.tsx
@@ -1,19 +1,31 @@
-import { CSSProperties, useState } from "react";
+import React, { CSSProperties, useCallback, useEffect, useMemo, useState } from "react";
+
+
+export type TActive = {
+    name: string
+    , active: boolean
+    , fnUpdActive: (nActive: boolean) => void
+}
 
 const Bar: React.FC<{
     dark: boolean
     , fnMute: (nMute?: boolean) => boolean
-}> = ({ dark, fnMute }) => {
-    const [show, setShow] = useState(false);
-
+    , actives: Array<TActive>
+}> = ({ dark, fnMute, actives }) => {
+    const [hovered, setHovered] = useState(false);
+    const [moved, setMoved] = useState(false);
     const [mute, setMute] = useState(fnMute());
+    const [moveTimeOut, setMoveTimeOut] = useState<NodeJS.Timeout | null>(null);
 
-    const btnCss: CSSProperties  = {
+    const timeOutSec: number = 5;
+
+    const btnCss: CSSProperties = useMemo(() => ({
         cursor: "pointer"
         , color: dark ? "white" : "black"
-    }
-    
-    const fnFullScreen = (e: React.MouseEvent) => {
+        , userSelect: "none"
+    }), [dark]);
+
+    const fnFullScreen = useCallback((e: React.MouseEvent) => {
         if (document.fullscreenElement) {
             document.exitFullscreen();
         }
@@ -21,19 +33,64 @@ const Bar: React.FC<{
             document.documentElement.requestFullscreen();
         }
         e.stopPropagation();
-    }
+    }, []);
 
-    const fnPlayOrMute = (e: React.MouseEvent) => {
+    const fnPlayOrMute = useCallback((e: React.MouseEvent) => {
         setMute(!mute);
         fnMute(!mute);
         e.stopPropagation();
-    }
+    }, [mute, fnMute]);
+
+    const fnUserMoved = useCallback(() => {
+        setMoved(true);
+
+        if (moveTimeOut) {
+            clearTimeout(moveTimeOut);
+        }
+
+        setMoveTimeOut(setTimeout(() => {
+            setMoved(false);
+
+            if (moveTimeOut) {
+                clearTimeout(moveTimeOut);
+            }
+        }, timeOutSec * 1000));
+    }, [moveTimeOut]);
+
+    useEffect(() => {
+        window.addEventListener("mousemove", fnUserMoved);
+
+        return () => {
+            if (moveTimeOut) {
+                window.removeEventListener("mousemove", fnUserMoved);
+                clearTimeout(moveTimeOut);
+            }
+        };
+    }, [fnUserMoved, moveTimeOut]);
+
+    useEffect(() => {
+        if (!hovered && !moved) {
+            document.body.style.cursor = "none";
+        }
+        else {
+            document.body.style.cursor = "auto";
+        }
+    }, [hovered, moved]);
 
     return (
-        <div className="h-10 min-w-10 flex flex-row gap-2 p-2" onMouseOver={() => setShow(true)} onMouseOut={() => setShow(false)}>
-            {show && [
-                <span key="fullscreen" className="material-icons" onClick={e => fnFullScreen(e)} style={btnCss}>fullscreen</span>
-                , <span key="audio" className="material-icons" onClick={e => fnPlayOrMute(e)} style={btnCss}>{mute ? "volume_up" : "volume_off"}</span>
+        <div className="min-h-10 min-w-10 flex flex-col gap-2 p-2" onMouseOver={() => setHovered(true)} onMouseOut={() => setHovered(false)}>
+            {(hovered || moved) && [
+                <div key="control" className="flex flex-row gap-2 items-start justify-start">
+                    <span className="material-icons" onClick={e => fnFullScreen(e)} style={btnCss}>fullscreen</span>
+                    <span className="material-icons" onClick={e => fnPlayOrMute(e)} style={btnCss}>{mute ? "volume_up" : "volume_off"}</span>
+                </div>
+                , <div key="actives" className="flex flex-row gap-2 items-start justify-start">
+                    {actives.map((active: TActive, idx: number) => (
+                        <span key={idx} className={`cursor-pointer select-none ${dark ? "text-white" : ""}`} onClick={(e) => { active.fnUpdActive(!active.active); e.stopPropagation(); }}>
+                            <input type="checkbox" defaultChecked={active.active} readOnly={true} /> {active.name}
+                        </span>
+                    ))}
+                </div>
             ]}
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,65 @@
 "use client"
 
-import { CSSProperties, useEffect, useState } from "react";
+import { CSSProperties, useCallback, useEffect, useMemo, useState } from "react";
 
-import Bar from "./Bar";
+import Bar, { TActive } from "./Bar";
 
-const bgs: Array<string> = ["#FF0000", "#00FF00", "#0000FF", "#FFFFFF", "#000000"] as const;
-const pattern: Array<string> = ["to left, white, black", "to right, white, black", "to bottom, white, black", "to top, white, black"] as const;
-const dark: Array<boolean> = [false, false, false, false, true, true, false, false, true] as const;
+
+type TStyles = {
+    [name: string]: { style: CSSProperties, topLeftDark: boolean }
+}
+
+const styles: TStyles = {
+    "red": { style: { backgroundColor: "#FF0000" }, topLeftDark: false }
+    , "green": { style: { backgroundColor: "#00FF00" }, topLeftDark: false }
+    , "blue": { style: { backgroundColor: "#0000FF" }, topLeftDark: false }
+    , "white": { style: { backgroundColor: "#FFFFFF" }, topLeftDark: false }
+    , "black": { style: { backgroundColor: "#000000" }, topLeftDark: true }
+
+    , "white-black-right-left": { style: { background: "linear-gradient(to left, white, black)" }, topLeftDark: true }
+    , "white-black-left-right": { style: { background: "linear-gradient(to right, white, black)" }, topLeftDark: false }
+    , "white-black-top-bottom": { style: { background: "linear-gradient(to bottom, white, black)" }, topLeftDark: false }
+    , "white-black-bottom-top": { style: { background: "linear-gradient(to top, white, black)" }, topLeftDark: true }
+};
 
 const Home: React.FC = () => {
     const [init, setInit] = useState(false);
-    
     const [bgIdx, setBgIdx] = useState(0);
-
     const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
-    const fnChgBg = () => {
-        if (init) {
-            setBgIdx((bgIdx + 1) % (bgs.length + pattern.length));
-        }
-    }
+    const styleNames = Object.keys(styles);
 
-    const fnMute = (nMute?: boolean): boolean => {
+    const [isActive, setIsActive] = useState(styleNames.map(() => true));
+
+    const actives: Array<TActive> = useMemo(() => (
+        styleNames.map((name: string, idx: number) => ({
+            name: name
+            , active: isActive[idx]
+            , fnUpdActive: (nActive: boolean) => {
+                setIsActive([
+                    ...isActive.slice(0, idx)
+                    , nActive
+                    , ...isActive.slice(idx + 1, isActive.length)
+                ])
+            }
+        }))
+    ), [isActive])
+
+    const fnChgBg = useCallback(() => {
+        if (init) {
+            let nIdx: number = bgIdx;
+
+            do {
+                nIdx = (nIdx + 1) % (styleNames.length);
+            } while (!isActive[nIdx] && nIdx !== bgIdx);
+
+            if (nIdx !== bgIdx) {
+                setBgIdx(nIdx);
+            }
+        }
+    }, [init, bgIdx, isActive]);
+
+    const fnMute = useCallback((nMute?: boolean): boolean => {
         if (audio) {
             if (nMute !== undefined) {
                 audio.muted = nMute;
@@ -31,13 +69,7 @@ const Home: React.FC = () => {
         }
 
         return false;
-    }
-
-    const bgStyle: CSSProperties = {
-        ...(!init && { backgroundColor: "#FFFFFF" })
-        , ...((init && bgIdx >= 0 && bgIdx < bgs.length) && { backgroundColor: bgs[bgIdx] })
-        , ...((init && bgIdx >= bgs.length && bgIdx < bgs.length + pattern.length) && { background: `linear-gradient(${pattern[bgIdx - bgs.length]})` })
-    }
+    }, [audio]);
 
     useEffect(() => {
         if (init) {
@@ -48,10 +80,23 @@ const Home: React.FC = () => {
         }
     }, [init]);
 
+    // Change background color on any key press
+    useEffect(() => {
+        window.addEventListener("keydown", fnChgBg);
+
+        return () => {
+            window.removeEventListener("keydown", fnChgBg);
+        };
+    }, [fnChgBg]);
+
     return (
-        <div className="w-full h-full" onClick={fnChgBg} style={bgStyle}>
-            {!init && <div className="w-full h-full flex items-center justify-center absolute" onClick={() => setInit(true)}>Start Testing</div>}
-            <Bar dark={dark[bgIdx]} fnMute={fnMute} />
+        <div id="bg" className="w-full h-full" onClick={fnChgBg} style={(init ? styles[styleNames[bgIdx]].style : {})}>
+            {!init && <div className="w-full h-full flex flex-col items-center justify-center gap-1 absolute" onClick={() => setInit(true)}>
+                <div className="text-2xl mb-4">Instructions</div>
+                <div>Click Anywhere to Change Background</div>
+                <div>Move Mouse to Show Controls</div>
+            </div>}
+            {init && <Bar dark={styles[styleNames[bgIdx]].topLeftDark} fnMute={fnMute} actives={actives} />}
         </div>
     );
 }
